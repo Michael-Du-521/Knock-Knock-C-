c96b77104d12425d0dfa66fcbd90aef355ac7d3a
Miguel_Du <13104391508@163.com>
2023-06-14 10:32:49 +0800

    1.2023年6月14日上午 C#笔记

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Convert.ToString(89, 2));
-            Console.ReadKey();
+            #region 
+            //Console.WriteLine(Convert.ToString(89, 2));
+            //Console.ReadKey();
+            //Console.WriteLine(Convert.ToString(25,2));
+            //Console.ReadKey();
+            //Console.WriteLine(Convert.ToString(1101,10));// unchange
+            //Console.ReadKey();
+            //Console.WriteLine(Convert.ToInt32("1101",2));// convert successfully
+            //Console.ReadKey();
+            #endregion
+
+            #region MyRegion
+            int a = 7;
+            int b = 2;
+            int sum, dif, pro, quo;
+            sum = a + b;
+            dif = a - b;
+            pro = a * b;
+            quo = a / b;
+            Console.WriteLine("{0}+{1}={2}", a, b, sum);
+            Console.WriteLine("{0}/{1}={2}", a, b, quo);
+            #endregion
+
+
+            #region MyRegion
+            int myAge = 24, myWeight = 65;
+            #endregion
         }
     }
 }
